2534aaf4455a9842f40f5cc2f35b9ed9e16469b4
isaaclem <lem.chiew.choon@gmail.com>
2018-07-15 10:35:30 +0800

    Added a search bar and performed YTSearch @ index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyA3nGzXiIJWtVTQsndzWgBekeJcF4waY6Q';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+})
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
